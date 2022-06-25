ad126e5c1234c35b2e7e838de5ebeb2f612e11fb
Wesley Mendonça <wmendonca@onlineapp.com.br>
2022-06-25 13:24:18 -0300

    Inicializando o repositorio com os arquivos iniciais do projeto

---
+++ src/test/java/dataFactory/ProdutoDataFactory.java
@@ -0,0 +1,38 @@
+package dataFactory;
+
+import pojo.ComponentePojo;
+import pojo.ProdutoPojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoDataFactory {
+    public static ProdutoPojo criarProdutoComumComOValorIgualA (double valor) {
+        ProdutoPojo produto = new ProdutoPojo();
+        produto.setProdutoNome("PS5");
+        produto.setProdutoValor(valor);
+
+        List<String> cores = new ArrayList<>();
+        cores.add("preto");
+        cores.add("branco");
+
+        produto.setProdutoCores(cores);
+        produto.setProdutoUrlMock("");
+
+        List<ComponentePojo> componentes = new ArrayList<>();
+
+        ComponentePojo primeiroComponente = new ComponentePojo();
+        primeiroComponente.setComponenteNome("controles");
+        primeiroComponente.setComponenteQuantidade(2);
+        ComponentePojo segundoComponente = new ComponentePojo();
+        segundoComponente.setComponenteNome("jogos");
+        segundoComponente.setComponenteQuantidade(5);
+
+        componentes.add(primeiroComponente);
+        componentes.add(segundoComponente);
+
+        produto.setComponentes(componentes);
+
+        return produto;
+    }
+}
---
+++ src/test/java/dataFactory/UsuarioDataFactory.java
@@ -0,0 +1,15 @@
+package dataFactory;
+
+import pojo.UsuarioPojo;
+
+public class UsuarioDataFactory {
+    public static UsuarioPojo criarUsuarioComum (String login, String senha) {
+
+        UsuarioPojo usuario = new UsuarioPojo();
+
+        usuario.setUsuarioLogin(login);
+        usuario.setUsuarioSenha(senha);
+
+        return usuario;
+    }
+}
